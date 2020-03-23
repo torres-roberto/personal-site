3a505bb3ff839438ece740726f74570801dd7a23
Roberto Torres <jr.torres.roberto@gmail.com>
2020-03-23 00:32:35 -0500

    and now the disappearing profile act

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-function hey() {
-    console.log('hello world');
-}
-
-hey();
+document.addEventListener('DOMContentLoaded', (event) => {    
+    let display = true;
+    let profile = document.querySelector(".profile");
+    let profileArea = document.querySelector('#profile-pic');
+    
+    profileArea.addEventListener('click', (e) => {
+        if (display === true) {
+            profile.classList.add("hidden-profile");
+            display = false;
+        }
+        else if (display === false) {
+            profile.classList.remove("hidden-profile");
+            display = true;
+        }
+    });
+});
